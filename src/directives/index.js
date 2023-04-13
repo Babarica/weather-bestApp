18da561294dfaafe7b2f7f5f6e5e60f770e173c3
ilyaboy <ilya_lavrov_2003@list.ru>
2023-04-13 17:26:59 +0300

    adding true dist

--- src/directives/index.js
+++
@@ -1,2 +0,0 @@
-import VFocus from "./VFocus";
-export default [VFocus];
--- vue.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  lintOnSave: false,
-
-  chainWebpack: (config) => {
-    config.resolve.alias.set("vue-slick", "vue-slick/dist/vue-slick.js");
-  },
-};
